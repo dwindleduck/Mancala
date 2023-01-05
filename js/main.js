788a3547dddc61428248f986e1a1746567dcc44d
Andrew Squier <andrewsquier@Andrews-MacBook-Pro.local>
2023-01-04 23:41:12 -0500

    capture functionality on landing in empty space on your side of the board

--- js/main.js
+++ js/main.js
@@ -94,9 +94,8 @@ class GameBoard {
 
 
 
-
+    
     takeTurn(pitID) {
-        console.log(pitID)
         let selectedPit = undefined
         let indexOfPit = 0
         let mySideOfTheBoard
@@ -138,27 +137,71 @@ class GameBoard {
         for(let i=selectedPit.contents.length; i>0; i--){
             this.stonesList.push(selectedPit.contents.pop())
             selectedPit.render()
+            //need to add some animation stuff here or in the render() function
         }
 
-        //need to add some animation stuff here
+        
 
 
+        //deposit the stones
         //starting counterclockwise, deposit one stone into each pit, skipping opponent's store
         while(this.stonesList.length>0) {
+            
             //WHILE there are stones in holding and we are still on our side of the board
             while(this.stonesList.length>0 && indexOfPit < 6){
                 mySideOfTheBoard[indexOfPit].contents.push(this.stonesList.pop())
                 mySideOfTheBoard[indexOfPit].render()
+
+                //IF LAST STONE is dropped in an empty space on my side
+                if(this.stonesList.length === 0 && mySideOfTheBoard[indexOfPit].contents.length === 1){
+                    //get the last stone back from our pit
+                    this.stonesList.push(mySideOfTheBoard[indexOfPit].contents.pop())
+                    mySideOfTheBoard[indexOfPit].render()
+                
+                    
+                    //Capture stones in opposite space if they are there
+                   let indexOfOppositePit = Math.abs(indexOfPit-5)
+
+                    for(let i = otherSideOfTheBoard[indexOfOppositePit].contents.length; i>0; i--){
+                        this.stonesList.push(otherSideOfTheBoard[indexOfOppositePit].contents.pop())
+                    }
+                    otherSideOfTheBoard[indexOfOppositePit].render()
+
+                    console.log(this.stonesList)
+                    console.log(this.stonesList.length)
+                    
+
+                    //Drop them in my store
+                    while(this.stonesList.length>0){
+                        myStore.contents.push(this.stonesList.pop())
+                    }
+                    console.log(this.stonesList)
+                    myStore.render()
+
+                    //update notification area with "Captured!"
+                    //check for end of game
+                    this.player1Turn = !this.player1Turn
+                    return // ends the turn
+                } 
                 indexOfPit++
             }
             //After mySideOfTheBoard 
             //IF there are stones in holding, deposit one into my store
-            console.log(this.stonesList)
             if(this.stonesList.length>0){
                 myStore.contents.push(this.stonesList.pop())
                 myStore.render()
+
+                //IF last stone is dropped in my store --> take another turn
+                if(this.stonesList.length === 0){
+                    //Update Notification Area with "Free Turn!"
+                    //do not change the turn indicator
+                    //check for end of game
+                    return //ends the turn
+                }
                 indexOfPit = 0
             }
+            //After dropping a stone in my store
+            //If there are stones in holding, deposite on other side of the board
             while(this.stonesList.length>0 && indexOfPit < 6){
                 otherSideOfTheBoard[indexOfPit].contents.push(this.stonesList.pop())
                 otherSideOfTheBoard[indexOfPit].render()
@@ -167,23 +210,12 @@ class GameBoard {
             indexOfPit = 0 //reset to start again on my side of the board
             //omit the other player's store
         }
-
-
-
-
-        // console.log(this.stonesList)
-        // console.log(indexOfPit)
-
-
-        //IF last stone is in my store --> take another turn
-        //IF last stone is in an empty space on my side --> Capture stones in opposite space
-        
-
-        //update notification area
+        //check for end of game
         //change turn indicator, change dom display
+        this.player1Turn = !this.player1Turn
+    }//END OF TAKE TURN
 
 
-    }
 
 
 
@@ -236,6 +268,7 @@ class GameBoard {
         }
         //Now all stones are back in the stonesList, so redistribute them
         this.initialDistribution(4)
+        this.player1Turn = true
     }
 }
 
@@ -306,8 +339,8 @@ class Pit {
 //This creates new Pit objects, adds them to their array
 const createPits = (gameObject) => {
     //add to side2
-    for(let i=6; i>0; i--){
-        const pitObject = new Pit(`B${i}`)
+    for(let i=0; i<6; i++){
+        const pitObject = new Pit(`B${i+1}`)
         gameObject.side2.push(pitObject)
     }
     //add to side1
@@ -330,10 +363,9 @@ const createPits = (gameObject) => {
 
     //distribute stones
     gameObject.initialDistribution(4)
-    console.log(gameObject.side1)
-    console.log(gameObject.side2)
-    // console.log(gameObject.store1)
-    // console.log(gameObject.store2)
+    // console.log(gameObject.side1)
+    // console.log(gameObject.side2)
+
 }
 
 
