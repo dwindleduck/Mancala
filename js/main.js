60afe9c24003597f0cecf82fd103a0e7b4678887
Andrew Squier <andrewsquier@Andrews-MacBook-Pro.local>
2023-01-06 17:22:46 -0500

    code cleanup of white space, comments, unused code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-
-
 /*----- constants -----*/
 const gamesBucket = []
-let currentGameIndex
-
-
-
 
 /*----- state variables -----*/
-
-
-
+let currentGameIndex
 
 /*----- cached elements  -----*/
 const playArea = document.querySelector("#playArea")
@@ -27,11 +19,6 @@ const instructionsButton = document.querySelector("#instructionsButton")
 const instructions = document.querySelector("#instructions")
 const newGameButton = document.querySelector("#newGameButton")
 const resetButton = document.querySelector("#resetButton")
-
-
-
-
-
 
 /*----- event listeners -----*/
 //show/hide the instructions
@@ -64,17 +51,12 @@ newGameButton.addEventListener("click", () => {
     //gamesBucket.push(gameObject) //use this for multiple games 
     currentGameIndex = gamesBucket.indexOf(gameObject)
 
-
-
-    //Maybe this is an initialize function?
-
-
+    //Maybe below here is an initialize function?
     createPits(gamesBucket[currentGameIndex])
     gameBoard.style.display = "grid"
     gameTitle.innerText = gameObject.name
 
     player1Username.innerText = gameObject.p1Username
-    // console.log(player1Username)
     player1Username.classList.remove("hide")
     player2Username.innerText = gameObject.p2Username
     player2Username.classList.remove("hide")
@@ -83,56 +65,42 @@ newGameButton.addEventListener("click", () => {
     notificationArea.classList.remove("hide")
     turnIndicator.classList.remove("hide")
     turnIndicator.innerText = "Player 1's Turn"
-    // newGameButton.classList.add("hide")
     resetButton.classList.remove("hide")
 })
 
 //Reset Game Button
 resetButton.addEventListener("click", () => {
     gamesBucket[currentGameIndex].reset()
-
 })
 
-
-
-
-
-
 /*----- functions -----*/
-
-
 class GameBoard {
     constructor(player1name, player2name) {
         this.name = player1name + " v " + player2name
         this.p1Username = player1name
         this.p2Username = player2name
+        this.side1 = []
+        this.store1 = undefined
+        this.side2 = []
+        this.store2 = undefined
+        this.player1Turn = true
+        //for event listeners, this will be an array of the Pit DOM elements
+        this.pits = undefined 
 
         //create stonesList with unique identifiers
         //use stonesList as the 'holding' array during game play
         //also use during board reset
         this.stonesList = []
-        
         const colorList = ["red", "blue", "green", "purple"]
-
         for(let i=0; i<48; i++){
-            //need to attach color to the stone here
+            //create a stone, attach a name and a color
             let stone = {
                 name: `stone${i}`,
                 color: colorList[i%colorList.length]
             }
             this.stonesList.push(stone)
         }
-
-        this.side1 = []
-        this.store1 = undefined
-        this.side2 = []
-        this.store2 = undefined
-
-        this.player1Turn = true
-        this.pits = undefined //for event listeners
     }
-
-
 
     
     takeTurn(pitID) {
@@ -149,7 +117,6 @@ class GameBoard {
         if(this.player1Turn){
             //loop through side 1
             while(indexOfPit<this.side1.length && selectedPit === undefined){
-                //for(let i=0; i<this.side1.length; i++){
                 //IF the name of the current element matches the pitID
                 if(this.side1[indexOfPit].name === pitID){
                     selectedPit = this.side1[indexOfPit]
@@ -161,7 +128,6 @@ class GameBoard {
             }
         } else { //player2's turn
             while(indexOfPit<this.side1.length && selectedPit === undefined){
-                //for(let i=0; i<this.side2.length; i++){
                 if(this.side2[indexOfPit].name === pitID){
                     selectedPit = this.side2[indexOfPit]
                     mySideOfTheBoard = this.side2
@@ -183,9 +149,6 @@ class GameBoard {
             //need to add some animation stuff here or in the render() function
         }
 
-        
-
-
         //deposit the stones
         //starting counterclockwise, deposit one stone into each pit, skipping opponent's store
         while(this.stonesList.length>0) {
@@ -200,7 +163,6 @@ class GameBoard {
                     //get the last stone back from our pit
                     this.stonesList.push(mySideOfTheBoard[indexOfPit].contents.pop())
                     mySideOfTheBoard[indexOfPit].render()
-                
                     
                     //Capture stones in opposite space if they are there
                    let indexOfOppositePit = Math.abs(indexOfPit-5)
@@ -219,12 +181,7 @@ class GameBoard {
                     //update notification area with "Captured!"
                     notificationArea.innerHTML = `<p>Captured!</p>`
                     //animate fade away
-
-
-                    //check for end of game
-                    //this.checkForEndOfGame()
                     this.switchTurns()
-                    //this.player1Turn = !this.player1Turn
                     return // ends the turn
                 } 
                 indexOfPit++
@@ -239,11 +196,6 @@ class GameBoard {
                 if(this.stonesList.length === 0){
                     //Update Notification Area with "Free Turn!"
                     notificationArea.innerHTML = `<p>Free Turn!</p>`
-                
-
-                    //check for end of game
-                    //this.checkForEndOfGame()
-
                     //do not switchTurns()
                     return //ends the turn
                 }
@@ -259,14 +211,8 @@ class GameBoard {
             indexOfPit = 0 //reset to start again on my side of the board
             //omit the other player's store
         }
-        //check for end of game
-        //console.log("Game Over: " + this.checkForEndOfGame())
-        //change turn indicator, change dom display
         this.switchTurns()
-        // this.player1Turn = !this.player1Turn
     }//END OF TAKE TURN
-
-
 
 
     findSideTotal(side) {
@@ -314,6 +260,7 @@ class GameBoard {
             return false
         }
     }
+
 
     //called if end of game is true
     checkForWinner() {
@@ -408,10 +355,6 @@ class GameBoard {
 
 
 
-
-
-
-
 class Pit {
     constructor(name) {
         this.name = name
@@ -427,19 +370,19 @@ class Pit {
             <p class="stoneCount">${this.contents.length}</p>
             <ul class="stonesContainer"></ul>
             `
-        //add Stones to the DOM
+        //?????add Stones to the DOM
         // for(let i=0; i<this.contents.length; i++){
         //     const li = document.createElement("li")
-        //     // li.style.backgroundColor = this.contents[i].color    
         //     this.pitDisplay.querySelector(".stonesContainer").appendChild(li)
         // }
         gameBoard.appendChild(this.pitDisplay)
-        
     }
+
 
     isEmpty() {
        return this.contents.length === 0
     }
+
 
     //update the dom with stone count and stonecontainer
     render() {
@@ -471,25 +414,24 @@ class Pit {
 
 
 
-
-
 //called from the Event Listener on newGameButton
 //This creates new Pit objects, adds them to their array
 const createPits = (gameObject) => {
-    //add to side2
+      //Create Pits on side1
+      for(let i=0; i<6; i++){
+        const pitObject = new Pit(`A${i+1}`)
+        gameObject.side1.push(pitObject)
+    }
+    //Create Pits on side2
     for(let i=0; i<6; i++){
         const pitObject = new Pit(`B${i+1}`)
         gameObject.side2.push(pitObject)
-    }
-    //add to side1
-    for(let i=0; i<6; i++){
-        const pitObject = new Pit(`A${i+1}`)
-        gameObject.side1.push(pitObject)
     }
     //create stores
     gameObject.store1 = new Pit("Store1")
     gameObject.store2 = new Pit("Store2")
 
+    //Grab all Pits from the DOM, put them in an array
     gameObject.pits = document.querySelectorAll(".pit")
     
     //Event Listener's for pits
@@ -499,26 +441,6 @@ const createPits = (gameObject) => {
             if(gameObject.checkForEndOfGame()) {gameObject.checkForWinner()}
         })
     })
-
     //distribute stones
     gameObject.initialDistribution(4)
-    // console.log(gameObject.side1)
-    // console.log(gameObject.side2)
-
 }
-
-
-
-
-
-
-
-
-
-
-
-// initialize()
-
-// function initialize() {
-
-// }
