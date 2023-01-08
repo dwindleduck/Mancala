4da7f68a9ad3842257fadcea0e0195e9d6798d72
Andrew Squier <andrewsquier@Andrews-MacBook-Pro.local>
2023-01-08 15:47:44 -0500

    updates to README, some js cleanup

--- README.md
+++ README.md
@@ -1,42 +1,21 @@
 # Mancala
 
+The object of the game is to collect the most Stones in your Store by the end of the game
 
 
-
-
-
-![Screenshots](https://git.....)
+![Landing Page](/assets/landing-page.png)
+![New Game](/assets/newgame-page.png)
 ## Technology Used
 - Javascript
 - HTML
 - CSS 
 
----
 
 ## Getting Started 
-Play Mancala by following [this link](https://dwindleduck.github.io/Mancala/)
+- Play Mancala by following [this link](https://dwindleduck.github.io/Mancala/)
+- Click to "Start a New Game"
+- To make a move, click a Pit on your side of the board
 
-### Object of the game
-- To collect the most Stones in your Store by the end of the game
-
-### Game set up
-- Both sides of the board have six Pits
-- Each Pit starts with four Stones totaling 48 stones on the board
-- All the Stones are the same and are used interchangeably 
-- Facing their side of the board, each player's Store is to the right 
-
-### Game Play
-- To make a move, select a Pit from your side of the board and pick up all of the Stones inside
-- Starting counterclockwise from the selected Pit, deposit one Stone into each Pit - *including your Store but skipping your opponent's Store*
- - If the last Stone you drop is in your Store, you get another turn
- - If the last Stone you drop is in an empty Pit on your side of the board, you capture that Stone and all Stones in the opponent's Pit directly opposite - *captured Stones go to your Store*
- - The game ends when one side of the board is empty
- - If the game is over, the player with Stones left on their side of the board captures those Stones - *captured Stones go to your Store*
- - The player with the most Stones in their Store is the winner
-
-
-
----
 
 ## Next Steps
 
@@ -47,12 +26,11 @@ Play Mancala by following [this link](https://dwindleduck.github.io/Mancala/)
 - As a user, I want to be able to save a game to resume later
 - As a user, I want to select who goes first on the first game
 - As a user, I want who goes first to alternate every game
-
 - As a user, I want to be able to choose appearance of the stones
 - As a user, I want to be able to select my username
 - As a user, I want choose my avatar
 - As a user, I want see the avatar and username of who is playing
----
+
 ### Version 3
 - As a user, I want to be able to play against the computer
 - As a user, I want to choose the difficulty level when playing against the computer
--- js/main.js
+++ js/main.js
@@ -10,11 +10,9 @@ const gameBoard = document.querySelector("#gameBoard")
 const gameTitle = document.querySelector("#gameTitle")
 const player1Username = document.querySelector("#player1Username")
 const player2Username = document.querySelector("#player2Username")
-
 const notificationArea = document.querySelector("#notificationArea")
 const turnIndicator = document.querySelector("#turnIndicator")
 
-const moveSelector = document.querySelector("#moveSelector")
 const instructionsButton = document.querySelector("#instructionsButton")
 const instructions = document.querySelector("#instructions")
 const newGameButton = document.querySelector("#newGameButton")
@@ -44,34 +42,19 @@ instructionsButton.addEventListener("click", () => {
 
 //New Game Button
 newGameButton.addEventListener("click", () => {
-    //eventually get name as input, add functionality to switch between games
     gameBoard.innerHTML = ""
+    //eventually get usernames as input, add functionality to switch between games
     const gameObject = new GameBoard("Player 1", "Player 2") 
+    //gamesBucket.push(gameObject) //replace the next line for multiple games 
     gamesBucket[0] = gameObject
-    //gamesBucket.push(gameObject) //use this for multiple games 
     currentGameIndex = gamesBucket.indexOf(gameObject)
-
-    //Maybe below here is an initialize function?
-    createPits(gamesBucket[currentGameIndex])
-    gameBoard.style.display = "grid"
-    gameTitle.innerText = gameObject.name
-
-    player1Username.innerText = gameObject.p1Username
-    player1Username.classList.remove("hide")
-    player2Username.innerText = gameObject.p2Username
-    player2Username.classList.remove("hide")
-
-    notificationArea.innerHTML = ""
-    notificationArea.classList.remove("hide")
-    turnIndicator.classList.remove("hide")
-    turnIndicator.innerText = "Player 1's Turn"
-    resetButton.classList.remove("hide")
 })
 
 //Reset Game Button
 resetButton.addEventListener("click", () => {
     gamesBucket[currentGameIndex].reset()
 })
+
 
 /*----- functions -----*/
 class GameBoard {
@@ -100,9 +83,26 @@ class GameBoard {
             }
             this.stonesList.push(stone)
         }
+
+        this.createPits()
+        gameBoard.style.display = "grid"
+        gameTitle.innerText = this.name
+
+        player1Username.innerText = this.p1Username
+        player1Username.classList.remove("hide")
+        player2Username.innerText = this.p2Username
+        player2Username.classList.remove("hide")
+
+        notificationArea.innerHTML = ""
+        notificationArea.classList.remove("hide")
+        turnIndicator.classList.remove("hide")
+        turnIndicator.innerText = "Player 1's Turn"
+        resetButton.classList.remove("hide")
+
     }
 
     
+    //Called when a pit is clicked on
     takeTurn(pitID) {
         let selectedPit = undefined
         let indexOfPit = 0
@@ -113,7 +113,7 @@ class GameBoard {
         //when a pit is clicked on, remove old notification
         notificationArea.innerHTML = ""
 
-        //player1's turn
+        //if player1's turn, initialize the local variables
         if(this.player1Turn){
             //loop through side 1
             while(indexOfPit<this.side1.length && selectedPit === undefined){
@@ -138,9 +138,8 @@ class GameBoard {
             }
         }
         if(selectedPit === undefined) return // break out of this function if not your turn
-        //from here it is your turn,
-        //a pit has been selected,
-        //and the indexOfPit is +1 from the selected pit
+        //from here it is your turn, a pit has been selected,
+        //and the indexOfPit is +1 from the selected pit (couterclockwise)
 
         //move all stones from selected pit into holding array
         for(let i=selectedPit.contents.length; i>0; i--){
@@ -217,17 +216,15 @@ class GameBoard {
 
     findSideTotal(side) {
         let sideTotal = 0
-        for(let i=0; i<6; i++){ //loop side pits
-            sideTotal += side[i].contents.length
-        }
+        side.forEach(pit => {
+            sideTotal += pit.contents.length
+        })
         return sideTotal
     }
 
 
-    //check for end of game
-    //returns true if game is over
-    //else returns false
-    checkForEndOfGame() {
+    checkForEndOfGame() {//returns true if game is over, else returns false
+        //If Side1 is empty
         if(this.findSideTotal(this.side1) === 0) {
             //move all stones from side2 into holding then store2
             for(let j = 0; j<this.side2.length; j++) {
@@ -242,6 +239,7 @@ class GameBoard {
             this.store2.render()
             return true //game over
         } 
+        //Else If Side2 is emtpy
         else if(this.findSideTotal(this.side2) === 0) {
             //move all stones from side1 into holding then store1
             for(let j = 0; j<this.side1.length; j++) {
@@ -262,9 +260,10 @@ class GameBoard {
     }
 
 
-    //called if end of game is true
-    checkForWinner() {
+    checkForWinner() {//called if end of game is true
         turnIndicator.classList.add("hide")
+        player2Username.innerText = this.p2Username
+        player1Username.innerText = this.p1Username
 
         //IF Player1 has more than Player2
         if(this.store1.contents.length > this.store2.contents.length) {
@@ -291,33 +290,17 @@ class GameBoard {
         this.player1Turn = !this.player1Turn
         if(this.player1Turn) {
             turnIndicator.innerText = "Player 1's Turn"
+            player1Username.innerText = "your turn --  " + this.p1Username
+            player2Username.innerText = this.p2Username
         } else {
             turnIndicator.innerText = "Player 2's Turn"
+            player1Username.innerText = this.p1Username
+            player2Username.innerText = this.p2Username + "  -- your turn"
         }
     }
-
-
-    //this should only be called on an empty board
-    initialDistribution(numStones) {
-        //add to side1
-        for(let i=0; i<6; i++){
-            for(let j=0; j<numStones; j++){
-                this.side1[i].contents.push(this.stonesList.pop())
-            }
-            this.side1[i].render()
-        }
-        //add to side2
-        for(let i=0; i<6; i++){
-            for(let j=0; j<numStones; j++){
-                this.side2[i].contents.push(this.stonesList.pop())
-            }
-            this.side2[i].render()
-        }
-    }
-
 
     //Empties out all Pits, then calls initialDistribution
-    reset() {
+    reset() {   
         //Side 1: loop through array of pits
         for(let i=0; i<this.side1.length; i++){
             //loop through array of stones
@@ -346,9 +329,61 @@ class GameBoard {
         }
         //Now all stones are back in the stonesList, so redistribute them
         this.initialDistribution(4)
-        notificationArea.innerHTML = ""
-        this.player1Turn = true
-        turnIndicator.innerText = "Player 1's Turn"
+        // notificationArea.innerHTML = ""
+        // this.player1Turn = true
+        // turnIndicator.innerText = "Player 1's Turn"
+        this.player1Turn = false
+        this.switchTurns()
+
+    }
+
+    //this should only be called on an empty board (during setup, reset)
+    initialDistribution(numStones) {
+        //add to side1
+        for(let i=0; i<6; i++){
+            for(let j=0; j<numStones; j++){
+                this.side1[i].contents.push(this.stonesList.pop())
+            }
+            this.side1[i].render()
+        }
+        //add to side2
+        for(let i=0; i<6; i++){
+            for(let j=0; j<numStones; j++){
+                this.side2[i].contents.push(this.stonesList.pop())
+            }
+            this.side2[i].render()
+        }
+    }
+
+    //called from the GameBoard constructor
+    //This creates new Pit objects, adds them to their array
+    createPits(){
+        //Create Pits on side1
+        for(let i=0; i<6; i++){
+        const pitObject = new Pit(`A${i+1}`)
+        this.side1.push(pitObject)
+        }
+        //Create Pits on side2
+        for(let i=0; i<6; i++){
+            const pitObject = new Pit(`B${i+1}`)
+            this.side2.push(pitObject)
+        }
+        //create stores
+        this.store1 = new Pit("Store1")
+        this.store2 = new Pit("Store2")
+
+        //Grab all Pits from the DOM, put them in an array
+        this.pits = document.querySelectorAll(".pit")
+        
+        //Event Listener's for pits
+        this.pits.forEach(pit => {
+            pit.addEventListener("click", (e) => {
+                this.takeTurn(e.currentTarget.getAttribute("id"))
+                if(this.checkForEndOfGame()) {this.checkForWinner()}
+            })
+        })
+    //distribute stones
+    this.initialDistribution(4)
     }
 }
 
@@ -363,18 +398,12 @@ class Pit {
         //Initial Drawing
         this.pitDisplay = document.createElement("div")
         this.pitDisplay.classList.add("pit")
-        // this.pitDisplay.classList.add(this.name)
         this.pitDisplay.setAttribute("id", this.name)
         this.pitDisplay.innerHTML = `
             <p class="name">${this.name}</p>
             <p class="stoneCount">${this.contents.length}</p>
             <ul class="stonesContainer"></ul>
             `
-        //?????add Stones to the DOM
-        // for(let i=0; i<this.contents.length; i++){
-        //     const li = document.createElement("li")
-        //     this.pitDisplay.querySelector(".stonesContainer").appendChild(li)
-        // }
         gameBoard.appendChild(this.pitDisplay)
     }
 
@@ -392,10 +421,10 @@ class Pit {
 
         //stone container
         const stonesContainer = this.pitDisplay.querySelector(".stonesContainer")
-        //Add stones to the DOM
         
+        //Add stones to the DOM
         //IF nothing in the pit
-        if(this.contents.length === 0){
+        if(this.isEmpty()){
             //wipes out all <li>
             stonesContainer.innerHTML = ""
         }// Else IF not enough <li>
@@ -403,44 +432,9 @@ class Pit {
             //add <li> until the same as in the pit (this should handle +1 stone)
             for(let i=stonesContainer.childElementCount; i<this.contents.length; i++){
                 const li = document.createElement("li")
-                //**** check on the i logic here, might asign the wrong color?
                 li.style.backgroundColor = this.contents[i].color 
                 stonesContainer.appendChild(li)
             } 
         }
     }
-}
-
-
-
-
-//called from the Event Listener on newGameButton
-//This creates new Pit objects, adds them to their array
-const createPits = (gameObject) => {
-      //Create Pits on side1
-      for(let i=0; i<6; i++){
-        const pitObject = new Pit(`A${i+1}`)
-        gameObject.side1.push(pitObject)
-    }
-    //Create Pits on side2
-    for(let i=0; i<6; i++){
-        const pitObject = new Pit(`B${i+1}`)
-        gameObject.side2.push(pitObject)
-    }
-    //create stores
-    gameObject.store1 = new Pit("Store1")
-    gameObject.store2 = new Pit("Store2")
-
-    //Grab all Pits from the DOM, put them in an array
-    gameObject.pits = document.querySelectorAll(".pit")
-    
-    //Event Listener's for pits
-    gameObject.pits.forEach(pit => {
-        pit.addEventListener("click", (e) => {
-            gameObject.takeTurn(e.currentTarget.getAttribute("id"))
-            if(gameObject.checkForEndOfGame()) {gameObject.checkForWinner()}
-        })
-    })
-    //distribute stones
-    gameObject.initialDistribution(4)
 }
